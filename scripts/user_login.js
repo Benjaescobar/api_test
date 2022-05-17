ff58674b898ccbd3fae31c0de96445d4ec1eca97
Benjamin Escobar <bagster@200-105-75-eduroam-al.wlan.net.uc.cl>
2022-05-17 12:28:44 -0400

    git init

---
+++ scripts/user_login.js
@@ -0,0 +1,38 @@
+
+async function createUser(){
+    dataSignUp = {
+        email: "nombre6@email.com",
+        password: "123456",
+        firstName: "nombre",
+        lastName: "apellido"
+    }
+
+    const userCreated = await fetch("http://localhost:3000/api/users", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(dataSignUp)
+        }).then(respuesta => {
+            console.log(respuesta.statusText)
+            
+            return respuesta.json()
+        });  
+}
+
+async function getToken(){
+    dataLogIn = {
+        email: "nombre6@email.com",
+        password: "123456"
+    }
+
+    const tokenCreated = await fetch("http://localhost:3000/api/sessions", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(dataLogIn)
+        }).then(respuesta => {
+            console.log(respuesta.statusText)
+            return respuesta.text()
+        });
+    console.log(tokenCreated);
+
+    return tokenCreated
+}
